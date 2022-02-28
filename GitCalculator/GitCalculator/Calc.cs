967f0e604732aeb7657f4d83befa95ba6260ff27
moaslak <moaslak@gmail.com>
2022-02-28 19:01:56 +0100

    first commit

---
+++ GitCalculator/GitCalculator/Calc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCalculator
+{
+    internal class Calc
+    {
+        public double plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double div(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.Write("CANNOT DIVIDE BY ZERO");
+                return 0;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ GitCalculator/GitCalculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitCalculator; 
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            menuClass menu = new menuClass();
+        }
+
+    }
+}
