057320c72ecdf7d8a02d47a96a220b95868ef987
moaslak <moaslak@gmail.com>
2022-03-02 10:07:45 +0100

    xml comments

--- GitCalculator/GitCalculator/Calc.cs
+++ GitCalculator/GitCalculator/Calc.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace GitCalculator
 {
+    /// <summary>
+    /// A simple calculator that can do the four main aririthimic functions for to doubles.
+    /// </summary>
     internal class Calc
     {
+        /// <summary>
+        /// Addition function, a+b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>double</returns>
         public double plus(double a, double b)
         {
             double result = a + b;
@@ -15,6 +24,12 @@ namespace GitCalculator
             return result;
         }
 
+        /// <summary>
+        /// subtraction function, a-b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>double</returns>
         public double minus(double a, double b)
         {
             double result = a - b;
@@ -22,13 +37,27 @@ namespace GitCalculator
             return result;
         }
 
+        /// <summary>
+        /// Multiplication function, a*b
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>double</returns>
         public double multiply(double a, double b)
         {
             double result = a * b;
             Console.WriteLine("Result of multiplication is: " + result);
             return result;
         }
-
+        /// <summary>
+        /// Divide function, a/b.
+        /// </summary>
+        /// <remarks>
+        /// Inputing b=0, will return 0
+        /// </remarks>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>double</returns>
         public double div(double a, double b)
         {
             double result = 0;
--- GitCalculator/GitCalculator/InputHandler.cs
+++ GitCalculator/GitCalculator/InputHandler.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace GitCalculator
 {
+    /// <summary>
+    /// Takes strings from Console.ReadLine(), and converts these to different numberic datatypes.
+    /// </summary>
     internal class InputHandler
     {
+        /// <summary>
+        /// Reads a line input and converts it to a double.
+        /// </summary>
+        /// <remarks>When calling the function, it will loop until it receives an input that can be parsed to a double.
+        /// The function print the input argument to the console.
+        /// </remarks>
+        /// <param name="prompt"></param>
+        /// <returns>double</returns>
         public double getDouble(string prompt)
         {
             double number = 0;
@@ -19,6 +30,14 @@ namespace GitCalculator
             return number;
         }
 
+        /// <summary>
+        /// Reads a line input and converts it to a int.
+        /// </summary>
+        /// <remarks>When calling the function, it will loop until it receives an input that can be parsed to a int.
+        /// The function print the input argument to the console.
+        /// </remarks>
+        /// <param name="prompt"></param>
+        /// <returns>int</returns>
         public int getInt(string prompt)
         {
             int number = 0;
